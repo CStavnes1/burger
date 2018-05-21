27c867f27eacb0e1b1aa39e7169724654139ff28
ChrisStavnes <ChrisStavnes@gmail.com>
2018-05-21 16:01:35 -0500

    homework complete

--- config/orm.js
+++ config/orm.js
@@ -1,35 +1,43 @@
 
-var connection = require("../config/connection.js");
+//Import (require) connection.js into orm.js
+var connection = require('../config/connection.js')
 
+var orm =
+  {
 
-var orm = {
-    selectAll: function(cb) {
-      connection.query("SELECT * FROM burgers", function(err, result) {
+    //selectAll()
+
+    selectAll: function (callback) {
+      //mySQL Query
+      connection.query('SELECT * FROM burgers', function (err, result) {
         if (err) throw err;
-        cb(result);
+        callback(result);
       });
     },
 
-    insert: function(burger_name, cb) {
-        connection.query("INSERT INTO burgers SET ?", 
-        { burger_name: burger_name,
-        devoured: false,
-        },
-        function(err, result) {
-          if (err) throw err;
-          cb(result);
-        });
-      },
-
-    update: function(burgerID, cb) {
-        connection.query("INSERT INTO burgers SET ? WHERE?", [{devoured:true}, {id: burgerID}],
-        function(err, result)
+    //insertOne()
+    insertOne: function (burger_name, callback) {
+      connection.query('INSERT INTO burgers SET ?',
         {
-            if (err) throw err;
-            cb(result);
+          burger_name: burger_name,
+          devoured: false,
+        }, function (err, result) {
+          if (err) throw err;
+          callback(result);
+        });
+
+    },
+
+    //updateOne()
+    updateOne: function (burgerID, callback) {
+      connection.query('UPDATE burgers SET ? WHERE ?', [{ devoured: true }, { id: burgerID }],
+        function (err, result) {
+          if (err) throw err;
+          callback(result);
         });
     }
+  };
 
-}
-  
+
+// Export the ORM object in module.exports.
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,2 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser")
+
+// Node Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+// Open Server
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+
+
+app.listen(PORT, function() 
+{
+  console.log("App listening on PORT " + PORT);
+});
